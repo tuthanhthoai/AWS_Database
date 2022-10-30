0ba209f750b639950377d8faa4d955e557b37e0b
Admin <tuthanhthoai@gmail.com>
2022-10-30 20:19:07 +0700

    commit

--- insertAurora.py
+++ insertAurora.py
@@ -2,7 +2,7 @@ import insertUtil as ut
 import MySQLdb
 
 
-conn = MySQLdb.connect(host='database-aurora.cluster-ctnk8ti4crcr.us-east-1.rds.amazonaws.com', user='admin', passwd='12345678', db='covid19_aurora', port=3306)
+conn = MySQLdb.connect(host='database-aurora.cluster-cg7idv92bcgv.us-east-1.rds.amazonaws.com', user='admin', passwd='12345678', db='covid19_aurora', port=3306)
 print('Open DB successfully')
 ut.insert(conn)
 ut.select(conn)
